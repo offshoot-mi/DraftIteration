705ea038efcb4b179a84acedaf475fbef4dc3e01
offshoot-mi <offshoot019@gmail.com>
2026-03-12 23:05:51 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://draftiteration.onrender.com',
   credentials: true
 }));
 app.use(helmet());
